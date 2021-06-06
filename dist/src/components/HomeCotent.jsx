eab027eb3271a7c8dd3b59e4ef2090bd8a408f44
naveengupta17 <naveen.gupta_cs18@gla.ac.in>
2021-06-06 18:46:05 +0530

    adding home series and movies page

---
+++ dist/src/components/HomeCotent.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import {NavLink} from "react-router-dom";
+import Series from "./../icons/Series.png"
+import Movies from "./../icons/Movies.png"
+import "./Content.css"
+
+function HomeContent() {
+    return (
+        <div>
+            
+        <div className="c_header">
+            <p>Popular Title</p>
+        </div>
+        <div className="c_content">
+        <NavLink to="/Series">
+        <div className="c_sub">
+            <img src={Series} alt="im" />
+            <p>Popular Series</p>
+        </div>
+        </NavLink>
+        <NavLink to="/Moviess">
+        <div className="c_sub">
+            <img src={Movies} alt="im" />
+            <p>Popular Movies</p>
+        </div>
+        </NavLink>
+        </div>
+       
+    </div>
+    
+        
+    )
+}
+
+export default HomeContent
